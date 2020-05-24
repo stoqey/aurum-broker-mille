38ae611b60025ef39a765a4dbb65d0c189720b75
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-05-23 21:02:30 -0400

    Broker HTTP server

---
+++ src/broker.http.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import os from 'os';
+import { log, verbose } from './log';
+import MilleBroker from '.';
+
+interface Args {
+    app: express.Application;
+    broker: MilleBroker;
+}
+
+const HOSTNAME = os.hostname();
+
+/**
+ * Simulated Mille Broker Server
+ * @param args 
+ * @param   app: express.Application; 
+ * @param   broker: MilleBroker;
+ */
+export const brokerHttpServer = (args: Args): express.Application => {
+
+    const { app, broker } = args;
+
+    // Health check endpoint
+    app.get('/', async (req, res) => {
+        verbose('/health')
+        res.json({ time: new Date(), hostname: os.hostname() })
+    });
+
+    app.post('/order/enter', async (req, res) => {
+        broker.enterPosition(req.body);
+        log('/order/enter', req.body && req.body.symbol)
+        res.json({ time: new Date(), HOSTNAME, status: 200 });
+    });
+
+    app.post('/order/exit', async (req, res) => {
+        broker.exitPosition(req.body);
+        log('/order/exit', req.body && req.body.symbol)
+        res.json({ time: new Date(), HOSTNAME, status: 200 });
+    });
+
+    return app;
+}
