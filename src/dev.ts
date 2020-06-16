a9a098815c23021c892f11c526ec43c6ca18d916
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-06-16 11:58:26 -0400

    hour format

--- src/dev.ts
+++ src/dev.ts
@@ -1,7 +1,7 @@
 import { MilleBroker } from '.';
 
 const startDate = new Date("2020-03-10 09:30:00");
-const milleBroker = new MilleBroker(startDate, { write: false, resume: true });
+const milleBroker = new MilleBroker(startDate, { write: true, resume: true });
 
 milleBroker.when('onReady', async () => {
     console.log('on ready');
